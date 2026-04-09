82e301485f2a1399bf395bd73bced5fdb5c1bf0c
Mohammed Sameer <rskkhan89@gmail.com>
2026-04-09 10:05:47 +0530

    update

--- qna/src/shared/database/name.ts
+++
@@ -1,9 +0,0 @@
-
-
-export const db = "qna-database"
-export const questionCollection = "questions"
-export const answerCollection = "answers"
-export const userCollection = "users"
-export const voteCollection = "votes"
-export const commentCollection = "comments"
-export const questionAttachmentCollection = "question-attachments"
