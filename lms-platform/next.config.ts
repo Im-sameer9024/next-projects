3bc241ba1bda1c5d325c98d3548ce14123c95c4b
Mohammed Sameer <rskkhan89@gmail.com>
2026-04-19 15:10:56 +0530

    build update

--- lms-platform/next.config.ts
+++ lms-platform/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-   allowedDevOrigins: ['192.168.1.25'],
+   allowedDevOrigins: ['192.168.1.24'],
    images: {
     remotePatterns: [
       {
